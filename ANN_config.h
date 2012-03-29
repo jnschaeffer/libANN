32e7922a15869687c8464deaa5a658b3c4356b9e
John Schaeffer <jnschaeffer@gmail.com>
2012-03-29 17:00:56 -0400

    ...

--- ANN_config.h
+++
@@ -1,8 +0,0 @@
-//config.h.in for libANN to be used by CMake.
-#ifndef _ANN_CONFIG_H
-#define _ANN_CONFIG_H
-#define HAVE_STDLIB_H
-#define HAVE_TIME_H
-#define HAVE_MATH_H
-#define HAVE_STDIO_H
-#endif
